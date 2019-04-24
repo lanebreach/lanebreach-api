6d6f28c5b1beb62cb535e488937d564d85d17ad4
Alex Wood <alex.wood.86@gmail.com>
2019-04-24 15:56:24 -0700

    + REST endpoints for retrieving ridership counts

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 require 'sidekiq/web'
+
 unless Rails.env.development?
   Sidekiq::Web.use Rack::Auth::Basic do |username, password|
     ActiveSupport::SecurityUtils.secure_compare(::Digest::SHA256.hexdigest(username), ::Digest::SHA256.hexdigest(ENV['SIDEKIQ_USERNAME'])) &
@@ -9,16 +10,22 @@ end
 Rails.application.routes.draw do
   mount Sidekiq::Web, at: '/sidekiq'
 
-  root :to => proc { [200,
-    {"Content-Type" => "text/html"},
-    ['Welcome to Lane Breach\'s api. Learn more at https://github.com/lanebreach/lanebreach-api.']] }
+  root :to => proc {
+    [
+      200,
+      { "Content-Type" => "text/html" },
+      ['Welcome to Lane Breach\'s api. Learn more at https://github.com/lanebreach/lanebreach-api.']
+    ]
+  }
 
   post "/graphql", to: "graphql#execute"
 
   namespace :api, defaults: { format: 'json' } do
     get '/bikeway_networks', to: 'bikeway_networks#nearest_network'
+
     resources :sf311_case_metadata, only: [:index]
     resources :sf311_cases, only: [:index, :create]
-    resources :bikeway_networks, only: [:show]
-  end  
+    resources :bikeway_networks, only: [:index, :show]
+    resources :sf_mta_ridership_counts, only: [:index]
+  end
 end
