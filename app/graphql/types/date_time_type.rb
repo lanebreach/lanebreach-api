481e65a6190d9bab1b7a7b4e27dd959cc8c2aabc
sahil agarwal <sahildagarwal@gmail.com>
2019-06-21 10:22:46 -0700

    Update BikewayNetworksController#nearest_network action to return all fields, and update BikewayNetwork datatypes (#33)

--- app/graphql/types/date_time_type.rb
+++ app/graphql/types/date_time_type.rb
@@ -2,5 +2,8 @@ Types::DateTimeType = GraphQL::ScalarType.define do
   name 'DateTime'
 
   coerce_input ->(value, _ctx) { Time.zone.parse(value) }
-  coerce_result ->(value, _ctx) { value.utc.iso8601 }
+  coerce_result ->(value, _ctx) { 
+    return value.iso8601 if value.instance_of?(Date)
+    value.utc.iso8601
+  }
 end
